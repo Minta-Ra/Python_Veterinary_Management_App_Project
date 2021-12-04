804c09309178c266affb86a9dcc6a7f5fcca5982
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-12-04 20:55:49 +0000

    Update vet details added

--- controllers/vet_controller.py
+++ controllers/vet_controller.py
@@ -18,13 +18,21 @@ def vets():
     vets = vet_repository.select_all()
     return render_template("vets/index.html", vets=vets)
 
+#################################################################
+#########  owners=owners, pets=pets - maybe not needed???
 # NEW
 # GET '/vets/new'
+# @vets_blueprint.route("/veterinarians/new", methods=["GET"])
+# def new_vet():
+#     owners = owner_repository.select_all()
+#     pets = pet_repository.select_all()
+#     return render_template("vets/new.html", owners=owners, pets=pets)
+
+
 @vets_blueprint.route("/veterinarians/new", methods=["GET"])
 def new_vet():
-    owners = owner_repository.select_all()
-    pets = pet_repository.select_all()
-    return render_template("vets/new.html", owners=owners, pets=pets)
+    return render_template("vets/new.html")
+
 
 # CREATE
 # POST '/vets'
@@ -36,22 +44,30 @@ def add_new_vet():
     vet_repository.save(vet)
     return redirect("/veterinarians")
 
-
-
-
 # SHOW
 # GET '/vets/<id>'
-# @vets_blueprint.route("/veterinarians/<id>")
-# def show(id):
-#     vet = vet_repository.select(id)
-#     owners = owner_repository.owners(vet)
-#     return render_template("vets/show.html", vet=vet, owners=owners)
+@vets_blueprint.route("/veterinarians/<id>")
+def show(id):
+    vet = vet_repository.select(id)
+    return render_template("vets/show.html", vet=vet)
 
 # EDIT
 # GET '/vets/<id>/edit'
+@vets_blueprint.route("/veterinarians/<id>/edit")
+def edit_vet(id):
+    vet = vet_repository.select(id)
+    return render_template("vets/edit.html", vet=vet)
 
 # UPDATE
 # PUT '/vets/<id>'
+@vets_blueprint.route("/veterinarians/<id>", methods=["POST"])
+def update_vet_details(id):
+    name = request.form["name"]
+    experience = request.form["experience"]
+    vet = Vet(name, experience, id)
+    vet_repository.update(vet)
+    return redirect("/veterinarians")
+
 
 # DELETE
 # DELETE '/vets/<id>'
